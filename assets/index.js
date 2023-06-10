8b1059b0cff7f21c9c4c40166b47fd88947635bf
unknown <dcwilkinson@wilktech.com>
2023-06-10 18:08:08 -0400

    added fuctionality for boards

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,29 @@
 const calculateWaferMc = (event) => {
-    console.log(event)
-    event.preventDefault();
+    // event.preventDefault();
     const WaferGreen = document.getElementById("GreenWafer").value;
     const WaferDry = document.getElementById("DryWafer").value;
     const WaferMC = document.getElementById("MoistureContent");
     const MC = (((WaferGreen - WaferDry) / WaferDry) * 100).toFixed(2);
 
     WaferMC.value = MC;
+    return MC / 100;
+
+}
+
+const calculateoODWeight = (event) => {
+    event.preventDefault();
+    const BoardGreen = document.getElementById("GreenBoard").value;
+    const BoardOvenDry = document.getElementById("COvenDryBoard");
+    const BoardMC = document.getElementById("BoardMC");
+
+    const WaferMC = calculateWaferMc();
+    console.log(WaferMC + 1);
+    const CalcOvenDry = (BoardGreen / (WaferMC + 1)).toFixed(2);
+    console.log(CalcOvenDry);
+    const CalcBoardMC = (((BoardGreen - CalcOvenDry) / CalcOvenDry) * 100).toFixed(2);
+    console.log(CalcBoardMC)
+
+    BoardOvenDry.value = CalcOvenDry;
+    BoardMC.value = CalcBoardMC
 
 }
